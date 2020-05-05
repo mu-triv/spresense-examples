2e76c3d359f646022612ddcfbad4b5ef2d6a827f
VU Khac Tri <khactri.vu@sony.com>
2020-05-05 17:26:26 +0200

    add new simple models

--- sketch_apr28a/sketch_apr28a.ino
+++ sketch_apr28a/sketch_apr28a.ino
@@ -1,10 +1,35 @@
 #include <dnnrt/runtime.h>
 
-const char bin_model[] PROGMEM = {
-#include "data/simple_nn_0.h"  
+const char bin_model_affine[] PROGMEM = {
+#include "data/simple_model_affine.h"  
 };
 
-const float input_data[] = {11,12,13,14,15};
+const char bin_model_relu[] PROGMEM = {
+#include "data/simple_model_relu.h"  
+};
+
+
+const char bin_model_sigmoid[] PROGMEM = {
+#include "data/simple_model_sigmoid.h"  
+};
+
+const char bin_model_tanh[] PROGMEM = {
+#include "data/simple_model_tanh.h"  
+};
+
+const char bin_model_softmax[] PROGMEM = {
+#include "data/simple_model_softmax.h"  
+};
+
+const float input_data[] = {-5,-2,0,4,6};
+
+const char* models[] = {
+  bin_model_affine,
+  bin_model_relu,
+  bin_model_sigmoid,
+  bin_model_tanh,
+  bin_model_softmax
+};
 
 nn_network_t *load_nnb_network(const char* bin_model)
 {
@@ -22,9 +47,6 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-
-  nn_network_t *network = load_nnb_network(bin_model);
-  
   /* initialize the whole dnnrt subsystem */
   const dnn_config_t config = {.cpu_num = 1 };
   int ret = dnn_initialize(&config);
@@ -32,42 +54,48 @@ void setup() {
       printf("dnn_initialize() failed due to %d", ret);
       goto dnn_error;
   }
-  printf("dnn_initialize() succeeded\n");
-
-  /* instantiate a neural network defined by nn_network_t as a dnn_runtime_t object */
-  dnn_runtime_t rt;
-  ret = dnn_runtime_initialize(&rt, network);
-  if (ret)  {
-      printf("dnn_runtime_initialize() failed due to %d\n", ret);
-      goto rt_error;
-    }
-  printf("dnn_runtime_initialize() succeeded\n");
-
-  int nbr_inputs = dnn_runtime_input_num(&rt);
-  int nbr_outputs = dnn_runtime_output_num(&rt);
-  printf("nbr_inputs=%d nbr_outputs=%d\n", nbr_inputs, nbr_outputs);
-  
-  nn_variable_t *in_var = dnn_runtime_input_variable(&rt, 0);
-  printf("var type = %d (NN_DATA_TYPE_FLOAT=%d)\n", in_var->type, NN_DATA_TYPE_FLOAT);
   printf("float size = %d", sizeof(float));
   printf("\n");
+  printf("dnn_initialize() succeeded\n");
 
-  const void *inputs[1] = { (const void *)input_data };
-  ret = dnn_runtime_forward(&rt, inputs, 1);
-  if (ret) {
-      printf("dnn_runtime_forward() failed due to %d\n", ret);
-      goto fin_error;
+  for (int i = 0; i < sizeof(models) / sizeof(models[0]); i++) {
+    printf("\n");
+    printf("Run the model #%d", i);
+    printf("\n");
+    nn_network_t *network = load_nnb_network(models[i]);
+    /* instantiate a neural network defined by nn_network_t as a dnn_runtime_t object */
+    dnn_runtime_t rt;
+    ret = dnn_runtime_initialize(&rt, network);
+    if (ret)  {
+        printf("dnn_runtime_initialize() failed due to %d\n", ret);
+        goto rt_error;
+      }
+    printf("dnn_runtime_initialize() succeeded\n");
+  
+    int nbr_inputs = dnn_runtime_input_num(&rt);
+    int nbr_outputs = dnn_runtime_output_num(&rt);
+    printf("nbr_inputs=%d nbr_outputs=%d\n", nbr_inputs, nbr_outputs);
+    
+    nn_variable_t *in_var = dnn_runtime_input_variable(&rt, 0);
+    printf("var type = %d (NN_DATA_TYPE_FLOAT=%d)\n", in_var->type, NN_DATA_TYPE_FLOAT);
+  
+    const void *inputs[1] = { (const void *)input_data };
+    ret = dnn_runtime_forward(&rt, inputs, 1);
+    if (ret) {
+        printf("dnn_runtime_forward() failed due to %d\n", ret);
+        goto fin_error;
+    }
+    printf("dnn_runtime_forward() succeeded\n");
+  
+    float* output_buffer = dnn_runtime_output_buffer(&rt, 0);
+    for (int i = 0; i < nbr_outputs; i++) {
+      printf("[output %d] %.4f\n", i, output_buffer[i]);
+    }
+  
+  fin_error:
+    /* free memories allocated to dnn_runtime_t */
+    dnn_runtime_finalize(&rt);
   }
-  printf("dnn_runtime_forward() succeeded\n");
-
-  float* output_buffer = dnn_runtime_output_buffer(&rt, 0);
-  for (int i = 0; i < nbr_outputs; i++) {
-    printf("[output %d] %.4f\n", i, output_buffer[i]);
-  }
-
-fin_error:
-  /* free memories allocated to dnn_runtime_t */
-  dnn_runtime_finalize(&rt);
 rt_error:
   /* finalize the whole dnnrt subsystem */
   dnn_finalize();
